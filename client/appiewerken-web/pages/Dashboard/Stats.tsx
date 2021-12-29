5c155fcd8a4c226e95284c0b16844ac3568de8e7
D22NK <daankentrop@gmail.com>
2021-12-29 14:59:28 +0100

    totaal bedrag stats pagina

--- client/appiewerken-web/pages/Dashboard/Stats.tsx
+++ client/appiewerken-web/pages/Dashboard/Stats.tsx
@@ -1,12 +1,30 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import MainLayout from "../../components/layouts/Main";
 import DashboardHeader from "../../components/DashboardHeader";
 export default function Stats() {
+  const [totaal, setTotaal] = useState<any>();
+  //   let winkels: any = [];
+  async function getWinkels() {
+    try {
+      const res = await axios.get("https://ahwapi.d22nk.nl/totaal");
+      console.log(res.data[0]._sum);
+      setTotaal(res.data[0]._sum);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getWinkels();
+  }, []);
   return (
     <MainLayout parentPage="Dashboard">
       <DashboardHeader page="Statistieken" />
       <div className="flex flex-row h-48">
         <div className="flex bg-slate-100 my-4 rounded-md p-4 w-[40%] mr-2">
-          Statiestiek 1
+          Totaal: € {totaal}
         </div>
 
         <div className="flex bg-slate-100 my-4 rounded-md p-4 w-[30%] mr-2">
