13ca73f981e29c9668eeada11b63bfd77b9c73b1
D22NK <daankentrop@gmail.com>
2021-12-30 12:46:44 +0100

    alltime totaal

--- server/src/routes/statRoutes.ts
+++ server/src/routes/statRoutes.ts
@@ -18,48 +18,55 @@ export default function statRoutes(prisma: any, app: Express) {
 
   app.get("/betalingstats", async (req: Request, res: Response) => {
     try {
-      const [totaal2020, totaal2021, totaal2022] = await prisma.$transaction([
-        prisma.betalingen.aggregate({
-          where: {
-            ontvangstdatum: {
-              gt: "2020-01-01T00:00:00.000Z",
-              lt: "2020-12-12T00:00:00.000Z",
+      const [totaal, totaal2020, totaal2021, totaal2022] =
+        await prisma.$transaction([
+          prisma.betalingen.aggregate({
+            _sum: {
+              bedrag: true,
             },
-          },
+          }),
 
-          _sum: {
-            bedrag: true,
-          },
-        }),
-
-        prisma.betalingen.aggregate({
-          where: {
-            ontvangstdatum: {
-              gt: "2021-01-01T00:00:00.000Z",
-              lt: "2022-12-12T00:00:00.000Z",
+          prisma.betalingen.aggregate({
+            where: {
+              ontvangstdatum: {
+                gt: "2020-01-01T00:00:00.000Z",
+                lt: "2020-12-12T00:00:00.000Z",
+              },
             },
-          },
 
-          _sum: {
-            bedrag: true,
-          },
-        }),
-
-        prisma.betalingen.aggregate({
-          where: {
-            ontvangstdatum: {
-              gt: "2022-01-01T00:00:00.000Z",
-              lt: "2023-12-12T00:00:00.000Z",
+            _sum: {
+              bedrag: true,
             },
-          },
+          }),
 
-          _sum: {
-            bedrag: true,
-          },
-        }),
-      ]);
+          prisma.betalingen.aggregate({
+            where: {
+              ontvangstdatum: {
+                gt: "2021-01-01T00:00:00.000Z",
+                lt: "2022-12-12T00:00:00.000Z",
+              },
+            },
 
-      res.json({ totaal2020 });
+            _sum: {
+              bedrag: true,
+            },
+          }),
+
+          prisma.betalingen.aggregate({
+            where: {
+              ontvangstdatum: {
+                gt: "2022-01-01T00:00:00.000Z",
+                lt: "2023-12-12T00:00:00.000Z",
+              },
+            },
+
+            _sum: {
+              bedrag: true,
+            },
+          }),
+        ]);
+
+      res.json({ totaal, totaal2020, totaal2021, totaal2022 });
     } catch (error) {
       console.log(error);
       res.sendStatus(500);
