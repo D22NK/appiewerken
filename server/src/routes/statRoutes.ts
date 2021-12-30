afe8f930ef401c97d2d91cd3cfe74c84ca333700
D22NK <daankentrop@gmail.com>
2021-12-30 12:37:31 +0100

    betaling stat test

--- server/src/routes/statRoutes.ts
+++ server/src/routes/statRoutes.ts
@@ -19,40 +19,42 @@ export default function statRoutes(prisma: any, app: Express) {
   app.get("/betalingstats", async (req: Request, res: Response) => {
     try {
       const [totaal2020, totaal2021, totaal2022] = await prisma.$transaction([
-        prisma.betalingen.aggregate({
-          _sum: {
-            bedrag: true,
-          },
+        prisma.betalingen.findMany({
           where: {
-            jaarWeek: {
-              contains: "2020",
+            ontvangstdatum: {
+              gt: "2020-07-24T00:00:00.000Z",
+              lt: "2021-12-14T00:00:00.000Z",
             },
           },
+
+          //   _sum: {
+          //     bedrag: true,
+          //   },
         }),
 
-        prisma.betalingen.aggregate({
-          _sum: {
-            bedrag: true,
-          },
-          where: {
-            jaarWeek: {
-              contains: "2021",
-            },
-          },
-        }),
-        prisma.betalingen.aggregate({
-          _sum: {
-            bedrag: true,
-          },
-          where: {
-            jaarWeek: {
-              contains: "2022",
-            },
-          },
-        }),
+        // prisma.betalingen.aggregate({
+        //   _sum: {
+        //     bedrag: true,
+        //   },
+        //   where: {
+        //     jaarWeek: {
+        //       contains: "2021",
+        //     },
+        //   },
+        // }),
+        // prisma.betalingen.aggregate({
+        //   _sum: {
+        //     bedrag: true,
+        //   },
+        //   where: {
+        //     jaarWeek: {
+        //       contains: "2022",
+        //     },
+        //   },
+        // }),
       ]);
 
-      res.json({ totaal2020, totaal2021, totaal2022 });
+      res.json({ totaal2020 });
     } catch (error) {
       console.log(error);
       res.sendStatus(500);
