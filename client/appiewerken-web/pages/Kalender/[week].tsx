8d2837ba24b377f654700c05b09f5582364d9ccf
D22NK <daankentrop@gmail.com>
2022-02-11 10:04:02 +0100

    Kalender week url

--- client/appiewerken-web/pages/Kalender/[week].tsx
+++ client/appiewerken-web/pages/Kalender/[week].tsx
@@ -1,8 +1,7 @@
-import { useState, useEffect } from "react";
 import MainLayout from "../../components/layouts/Main";
-import jaarWeekGen from "../../functions/jaarWeekGen";
-import axios from "axios";
 import { useRouter } from "next/router";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import dateformatter from "../../functions/dateformatter";
 import dagformatter from "../../functions/dagformatter";
 import daysBetween from "../../functions/daysBetween";
@@ -14,42 +13,28 @@ import {
   ArrowCircleRightIcon,
   ArrowNarrowLeftIcon,
 } from "@heroicons/react/outline";
-import Loader from "../../components/Loader";
-export default function Kalender() {
+export default function Dag() {
   const router = useRouter();
-
-  const [offset, setOffset] = useState(0);
-  const [jaarweek, setJaarweek] = useState(jaarWeekGen(offset));
   const [weekshifts, setWeekshifts] = useState<any>([]);
   const [formattedWeek, setFormattedWeek] = useState<any>();
   const [weekStats, setWeekStats] = useState<any>();
 
-  const [bericht, setBericht] = useState<String>("");
-  const [loading, setLoading] = useState(true);
   useEffect(() => {
-    getWeekShifts(jaarweek.jaar + "-" + jaarweek.week);
+    getWeekShifts();
   }, [router]);
 
   useEffect(() => {
     formatWeek();
     calculateStats();
   }, [weekshifts]);
-
-  async function getWeekShifts(params: any) {
+  async function getWeekShifts() {
     try {
-      setLoading(true);
-      setBericht("");
-      const res = await axios.get(`https://ahwapi.d22nk.nl/kalender/${params}`);
-      if (res.data.length < 1) {
-        setBericht("Er zijn voor deze week geen shifts gevonden");
-        setWeekshifts([]);
-      } else {
-        setWeekshifts(res.data);
-      }
-      setLoading(false);
+      const { week } = router.query;
+
+      const res = await axios.get(`https://ahwapi.d22nk.nl/kalender/${week}`);
+
+      setWeekshifts(res.data);
     } catch (error) {
-      setLoading(false);
-      setBericht("Er ging iets mis met het ophalen van deze week...");
       console.error(error);
     }
   }
@@ -93,39 +78,6 @@ export default function Kalender() {
     });
   }
 
-  function nextWeek() {
-    if (loading) {
-      return;
-    }
-    const newOffset = offset + 1;
-    setOffset(newOffset);
-    const newJaarWeek = jaarWeekGen(newOffset);
-    setJaarweek(newJaarWeek);
-    getWeekShifts(newJaarWeek.jaar + "-" + newJaarWeek.week);
-  }
-
-  function previousWeek() {
-    if (loading) {
-      return;
-    }
-    const newOffset = offset - 1;
-    setOffset(newOffset);
-    const newJaarWeek = jaarWeekGen(newOffset);
-    setJaarweek(newJaarWeek);
-    getWeekShifts(newJaarWeek.jaar + "-" + newJaarWeek.week);
-  }
-
-  function resetWeek() {
-    if (loading) {
-      return;
-    }
-    const newOffset = 0;
-    setOffset(newOffset);
-    const newJaarWeek = jaarWeekGen(newOffset);
-    setJaarweek(newJaarWeek);
-    getWeekShifts(newJaarWeek.jaar + "-" + newJaarWeek.week);
-  }
-
   function kalenderStyle(voltooid: Boolean, bcd: Boolean, ziek: Boolean) {
     let res;
     if (bcd) {
@@ -161,12 +113,11 @@ export default function Kalender() {
     });
     setWeekStats({ ug, ub, bedrag, ugv, ubv, bedragv });
   }
-
   return (
     <MainLayout parentPage="Kalender">
       <div className="flex flex-col">
         <h1 className="flex-1 text-2xl font-bold text-sky-500">
-          Kalender &middot; {jaarweek.jaar + "-" + jaarweek.week}
+          Kalender &middot; WEEK
         </h1>
         <div className="flex flex-col md:flex-row ">
           <div className="flex flex-1 flex-col bg-slate-200 m-2 p-2 rounded-md">
@@ -188,35 +139,6 @@ export default function Kalender() {
             </h2>
           </div>
         </div>
-        {loading && <Loader />}
-        {bericht !== "" && (
-          <div className="w-[100%] bg-slate-300 rounded-md p-2  mb-2 hover:bg-slate-400">
-            <p>{bericht}</p>
-          </div>
-        )}
-        <div className="flex flex-row">
-          <button
-            className="flex flex-row flex-1 items-center bg-sky-500 hover:border-sky-600 border-2 border-sky-500 hover:bg-transparent hover:text-sky-600 text-white font-bold text-xs py-1 px-2 rounded-md"
-            onClick={() => previousWeek()}
-          >
-            <ArrowCircleLeftIcon className="w-4 mr-2" /> Vorige week
-          </button>
-          {jaarWeekGen(0).jaar + "-" + jaarWeekGen(0).week !==
-            jaarweek.jaar + "-" + jaarweek.week && (
-            <button
-              className="ml-4 bg-slate-200 flex-1 font-bold text-xs py-1 px-2 rounded-md hover:bg-transparent border-slate-200 border-2"
-              onClick={() => resetWeek()}
-            >
-              Reset week
-            </button>
-          )}
-          <button
-            className=" flex flex-row flex-1 items-center justify-end ml-4 bg-sky-500 hover:border-sky-600 border-2 border-sky-500 hover:bg-transparent hover:text-sky-600 text-white font-bold text-xs py-1 px-2 rounded-md"
-            onClick={() => nextWeek()}
-          >
-            Volgende week <ArrowCircleRightIcon className="w-4 ml-2" />
-          </button>
-        </div>
 
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-7">
           {formattedWeek &&
@@ -236,7 +158,6 @@ export default function Kalender() {
                   formattedWeek[key].bcd,
                   formattedWeek[key].ziek
                 );
-                console.log(boxStyle, formattedWeek[key].dag, jaarweek.week);
                 return (
                   <Link
                     key={formattedWeek[key]?.id}
