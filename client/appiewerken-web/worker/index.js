41eea48fd16552307f5f415b4bb4daba094f5c8e
Daan Kentrop <43522330+D22NK@users.noreply.github.com>
2022-02-28 11:40:35 +0100

    schema update

--- client/appiewerken-web/worker/index.js
+++ client/appiewerken-web/worker/index.js
@@ -4,25 +4,14 @@ self.addEventListener("push", function onPush(event) {
       body: JSON.parse(event.data.text()).body,
       title: JSON.parse(event.data.text()).title,
       icon: "https://ahw.d22nk.nl/ah.png",
-      actions: [
-        { action: `shift-${JSON.parse(event.data.text()).shiftid}`, title: "Bekijk" },
-        { action: "shifts", title: "Alle Shifts" },
-      ],
+      
     })
   );
 });
 
 self.addEventListener("notificationclick", function (event) {
   event.notification.close();
-  if (event.action === "shifts") {
-    event.waitUntil(clients.openWindow("https://ahw.d22nk.nl/Shifts"));
-  } else if (event.action.includes("shift-")) {
-    event.waitUntil(
-      clients.openWindow(
-        `https://ahw.d22nk.nl/Shift/${event.action.split("-")[1]}`
-      )
-    );
-  } else {
+
     event.waitUntil(clients.openWindow("https://ahw.d22nk.nl/Shiftfollower"));
-  }
+  
 });
