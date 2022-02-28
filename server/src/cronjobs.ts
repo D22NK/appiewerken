6356583a9d1b77569449732a29b21545dec01c6f
Daan Kentrop <43522330+D22NK@users.noreply.github.com>
2022-02-28 13:28:54 +0100

    daily cronjob

--- server/src/cronjobs.ts
+++ server/src/cronjobs.ts
@@ -1,6 +1,39 @@
 import cron from "node-cron";
-export default function cronjobs() {
-  cron.schedule("0,30,15,45 * * * *", () => {
-    console.log("CRONJOB");
+import notify from "./utils/notify";
+import { PrismaClient } from "@prisma/client";
+
+export default async function cronjobs() {
+  const prisma = new PrismaClient();
+  const jaar = new Date().getFullYear().toString();
+  let maand = (new Date().getMonth() + 1).toString();
+  const dag = new Date().getDate().toString();
+  if (maand.length == 1) {
+    maand = "0" + maand;
+  }
+  const today = jaar + "-" + maand + "-" + dag + "T00:00:00.000Z";
+
+  cron.schedule("0 7 * * *", async () => {
+    const subs = await prisma.notificationSubscribers.findMany();
+
+    const shift = await prisma.shifts.findMany({
+      where: {
+        datum: today,
+      },
+      include: {
+        tijdslot: true,
+      },
+    });
+
+    if (shift.length == 1) {
+      subs.forEach((sub: any) => {
+        if (sub.daily) {
+          notify(
+            "Shifts",
+            `Vandaag 1 shift voor ${shift[0].werknemer} van ${shift[0].tijdslot.begin} tot  ${shift[0].tijdslot.eind}`,
+            JSON.parse(sub.sub)
+          );
+        }
+      });
+    }
   });
 }
