53771a2cfba92371c4e7668a18d92179cff25a80
Daan Kentrop <43522330+D22NK@users.noreply.github.com>
2022-03-04 18:52:41 +0100

    shift status filter title

--- client/appiewerken-web/pages/Shifts/index.tsx
+++ client/appiewerken-web/pages/Shifts/index.tsx
@@ -270,7 +270,7 @@ export default function Shifts() {
               setOpenStatus(!openstatus);
             }}
           >
-            Statuscurrent
+            {status}
             <svg
               className="-mr-1 ml-2 h-5 w-5"
               xmlns="http://www.w3.org/2000/svg"
